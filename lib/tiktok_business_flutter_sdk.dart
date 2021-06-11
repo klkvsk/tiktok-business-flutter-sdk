add6258c277c105b7e992a7f391a6663b6643c6d
klkvsk <m@klkvsk.ru>
2021-06-11 20:23:06 +0300

    reduce API to only a common and essential methods of both iOS and Android parts

--- lib/tiktok_business_flutter_sdk.dart
+++ lib/tiktok_business_flutter_sdk.dart
@@ -12,70 +12,8 @@ class TikTokBusinessFlutterSdk {
   static const MethodChannel _channel =
       const MethodChannel('tiktok_business_flutter_sdk');
 
-  static Future<String> get accessToken async {
-    return await _channel.invokeMethod('getAccessToken');
-  }
-
-  static Future<String> get appId async {
-    return await _channel.invokeMethod('getAppId');
-  }
-
-  static Future<String> get apiAvailableVersion async {
-    return await _channel.invokeMethod('getApiAvailableVersion');
-  }
-
-  static Future<void> setApiAvailableVersion(String version) async {
-    return await _channel
-        .invokeMethod('setApiAvailableVersion', {'version': version});
-  }
-
-  static Future<bool> get networkSwitch async {
-    return await _channel.invokeMethod('getNetworkSwitch');
-  }
-
-  static Future<bool> get sdkGlobalSwitch async {
-    return await _channel.invokeMethod('getSdkGlobalSwitch');
-  }
-
-  static Future<void> setSdkGlobalSwitch(bool sdkGlobalSwitch) async {
-    return await _channel.invokeMethod(
-        'setSdkGlobalSwitch', {'sdkGlobalSwitch': sdkGlobalSwitch});
-  }
-
-  static Future<bool> get isGlobalConfigFetched async {
-    return await _channel.invokeMethod('isGlobalConfigFetched');
-  }
-
-  static Future<void> setGlobalConfigFetched(String version) async {
-    return await _channel.invokeMethod('setGlobalConfigFetched');
-  }
-
-  static Future<bool> get isGaidCollectionEnabled async {
-    return await _channel.invokeMethod('isGlobalConfigFetched');
-  }
-
-  static Future<bool> get isInitialized async {
-    return await _channel.invokeMethod('isInitialized');
-  }
-
-  static Future<bool> get isSystemActivated async {
-    return await _channel.invokeMethod('isSystemActivated');
-  }
-
-  static Future<String> get logLevel async {
-    return await _channel.invokeMethod('getLogLevel');
-  }
-
-  static Future<void> clearAll() async {
-    return await _channel.invokeMethod('clearAll');
-  }
-
   static Future<void> flush() async {
     return await _channel.invokeMethod('flush');
-  }
-
-  static Future<void> destroy() async {
-    return await _channel.invokeMethod('destroy');
   }
 
   static Future<void> logout() async {
@@ -107,23 +45,29 @@ class TikTokBusinessFlutterSdk {
     String appId,
     String accessToken,
     String logLevel = LOG_LEVEL_INFO,
-    bool disableAdvertiserIDCollection = false,
-    bool disableAutoEvents = false,
-    bool disableAutoStart = false,
-    bool disableInstallLogging = false,
-    bool disableLaunchLogging = false,
-    bool disableRetentionLogging = false,
+    bool android_disableAdvertiserIDCollection = false,
+    bool android_disableAutoEvents = false,
+    bool android_disableAutoStart = false,
+    bool android_disableInstallLogging = false,
+    bool android_disableLaunchLogging = false,
+    bool android_disableRetentionLogging = false,
   }) async {
     await _channel.invokeMethod("initializeSdk", {
       'appId': appId,
       'accessToken': accessToken,
       'logLevel': logLevel,
-      'disableAdvertiserIDCollection': disableAdvertiserIDCollection,
-      'disableAutoEvents': disableAutoEvents,
-      'disableAutoStart': disableAutoStart,
-      'disableInstallLogging': disableInstallLogging,
-      'disableLaunchLogging': disableLaunchLogging,
-      'disableRetentionLogging': disableRetentionLogging,
+      'android': {
+        'disableAdvertiserIDCollection': android_disableAdvertiserIDCollection,
+        'disableAutoEvents': android_disableAutoEvents,
+        'disableAutoStart': android_disableAutoStart,
+        'disableInstallLogging': android_disableInstallLogging,
+        'disableLaunchLogging': android_disableLaunchLogging,
+        'disableRetentionLogging': android_disableRetentionLogging,
+      },
+      'ios': {
+
+      }
+
     });
   }
 
